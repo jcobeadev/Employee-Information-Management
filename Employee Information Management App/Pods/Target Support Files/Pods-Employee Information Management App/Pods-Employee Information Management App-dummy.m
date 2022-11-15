2b1b2fa6b813bf32fbd0a20fe92ad73f6831be6f
jcobeadev <jcobea.dev@gmail.com>
2022-11-15 12:00:27 +0800

    Remove cocoapod dependencies and use Swift Package instead, to make easier for the reviewer to run the app

--- Employee Information Management App/Pods/Target Support Files/Pods-Employee Information Management App/Pods-Employee Information Management App-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Employee_Information_Management_App : NSObject
-@end
-@implementation PodsDummy_Pods_Employee_Information_Management_App
-@end
--- Employee Information Management App/Pods/Target Support Files/Pods-Employee Information Management AppTests/Pods-Employee Information Management AppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Employee_Information_Management_AppTests : NSObject
-@end
-@implementation PodsDummy_Pods_Employee_Information_Management_AppTests
-@end
